42f6601ff5cd588421a7cab67a9f1a7f0f44a4aa
friendly-machine-4891 <liamhanafee@gmail.com>
2020-07-30 20:03:22 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -2,17 +2,17 @@
 
 
 def say_hello_ten_times
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
 :say_hello_ten_times
 
